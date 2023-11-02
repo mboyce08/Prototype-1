a1d027811b4f2a218e8c7d001662624e6acf9c6a
mboyce08 <147552773+mboyce08@users.noreply.github.com>
2023-11-02 12:42:27 +0000

    Multiplayer?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,14 @@ public class PlayerController : MonoBehaviour
     private float speed = 15.0f;
     private float turnSpeed = 30.0f;
     private float horizontalInput;
-    private float verticalInput;
+    private float forwardInput;
+
+    //Public Variables
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +26,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //This is where we get player Input
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
 
         //We move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //We Turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
     }
 }
